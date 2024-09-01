c3731b84e546f8543611abc9cdd82e8f724a5646
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-01 04:23:39 +0300

    UPD ALL UI version 2

--- get_it_assignment/lib/data_layer/tweet_data.dart
+++ get_it_assignment/lib/data_layer/tweet_data.dart
@@ -1,9 +1,28 @@
 import 'package:get_it_assignment/model/tweet_model.dart';
+import 'package:get_storage/get_storage.dart';
 
 class TweetData {
   late List<TweetModel> tweets = [];
+  final box = GetStorage();
 
-  addTweet({required TweetModel tweet}) {
-    tweets.add(tweet);
+  TweetData() {
+    loadTweets();
+  }
+
+  addTweet(TweetModel value) {
+    tweets.add(value);
+    box.write("tweets", tweets);
+  }
+
+  removeTweet(int id) {
+    tweets.removeWhere((tweet) => tweet.id == id);
+  }
+
+  loadTweets() {
+    if (box.hasData("tweets")) {
+      for (var tweet in box.read("tweets")) {
+        tweets.add(TweetModel.fromJson(tweet));
+      }
+    }
   }
 }
--- get_it_assignment/lib/servers/dependency.dart
+++ get_it_assignment/lib/servers/dependency.dart
@@ -1,8 +1,9 @@
 import 'package:get_it/get_it.dart';
 import 'package:get_storage/get_storage.dart';
 import '../data_layer/tweet_data.dart';
+final locator = GetIt.instance;
 
-void setupData() async {
+Future<void> setup() async {
   await GetStorage.init();
-  GetIt.I.registerSingleton<TweetData>(TweetData());
+  locator.registerSingleton<TweetData>(TweetData());
 }
